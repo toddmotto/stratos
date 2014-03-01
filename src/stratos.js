03b761f61a8a3693cbc955295b38d2d46c186a00
Fabrice Weinberg <Fabrice@weinberg.me>
2014-03-01 18:25:16 +0100

    exports.remove is calling exports.has, don't do it twice.

--- src/stratos.js
+++ src/stratos.js
@@ -48,9 +48,7 @@
 
   exports.destroy = function (object) {
     for (var key in object) {
-      if (exports.has(object, key)) {
-        exports.remove(object, key);
-      }
+      exports.remove(object, key);
     }
   };
 
